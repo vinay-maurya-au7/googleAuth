895915589a5e5b7fb102e8d149d56d5e2411da8e
vinay.maurya-au7 <vinay.shermauryan@gmail.com>
2020-08-23 16:16:41 +0530

    Google auth login

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import GoogleAuth from "./GoogleAuth";
+
+function App() {
+  return (
+    <div className="App">
+      <GoogleAuth className="google"></GoogleAuth>
+   
+    </div>
+  );
+}
+
+export default App;
